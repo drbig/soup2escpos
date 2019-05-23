4b05ea864f1dcd7d834ca564a7c35ed22c0ccee8
Piotr S. Staszewski <p.staszewski@gmail.com>
2019-05-23 20:14:57 +0200

    Add basic barcode support

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 )
 
 const (
-	VERSION = `0.0.2`
+	VERSION = `0.1.0`
 )
 
 var build = `UNKNOWN` // injected via Makefile
@@ -20,18 +20,65 @@ type TagDefintion struct {
 	StartBytes      string
 	EndBytes        string
 	EatsNextNewline bool
+	ProcFunc        func(xml.StartElement) string
+}
+
+type ByteRange struct {
+	Min byte
+	Max byte
+}
+
+type BarcodeDefinition struct {
+	Code        string
+	LenMin      int
+	LenMax      int
+	ValidRanges []ByteRange
+}
+
+var BARCODES = map[string]BarcodeDefinition{
+	"upc":    {"\x00", 11, 12, []ByteRange{{48, 57}}},
+	"ean13":  {"\x02", 12, 13, []ByteRange{{48, 57}}},
+	"ean8":   {"\x03", 7, 8, []ByteRange{{48, 57}}},
+	"code39": {"\x04", 1, 0, []ByteRange{{48, 57}, {65, 90}, {32, 32}, {36, 37}, {43, 43}, {45, 47}}},
 }
 
 var ESCPOS = map[string]TagDefintion{
-	"b":      {"\x1b\x45\x01", "\x1b\x45\x00", false},
-	"u":      {"\x1b\x2d\x01", "\x1b\x2d\x00", false},
-	"uu":     {"\x1b\x2d\x02", "\x1b\x2d\x00", false},
-	"small":  {"\x1b\x4d\x01", "\x1b\x4d\x00", false},
-	"center": {"\x1b\x61\x01", "\n\x1b\x61\x00", true},
-	"right":  {"\x1b\x61\x02", "\n\x1b\x61\x00", true},
-	"tall":   {"\x1b\x21\x10", "\x1b\x21\x00", false},
-	"wide":   {"\x1b\x21\x20", "\x1b\x21\x00", false},
-	"huge":   {"\x1b\x21\x30", "\x1b\x21\x00", false},
+	"b":      {"\x1b\x45\x01", "\x1b\x45\x00", false, nil},
+	"u":      {"\x1b\x2d\x01", "\x1b\x2d\x00", false, nil},
+	"uu":     {"\x1b\x2d\x02", "\x1b\x2d\x00", false, nil},
+	"small":  {"\x1b\x4d\x01", "\x1b\x4d\x00", false, nil},
+	"center": {"\x1b\x61\x01", "\n\x1b\x61\x00", true, nil},
+	"right":  {"\x1b\x61\x02", "\n\x1b\x61\x00", true, nil},
+	"tall":   {"\x1b\x21\x10", "\x1b\x21\x00", false, nil},
+	"wide":   {"\x1b\x21\x20", "\x1b\x21\x00", false, nil},
+	"huge":   {"\x1b\x21\x30", "\x1b\x21\x00", false, nil},
+	"barcode": {"", "", false, func(e xml.StartElement) string {
+		name := getAttr(e, "mode", true)
+		mode, ok := BARCODES[name]
+		if !ok {
+			log.Fatalln("No supported barcode type:", name)
+		}
+		value := []byte(getAttr(e, "value", true))
+		if len(value) < mode.LenMin {
+			log.Fatalln("Value under minimum length of", mode.LenMin)
+		}
+		if mode.LenMax > 0 && (len(value) > mode.LenMax) {
+			log.Fatalln("Value over maximum length of", mode.LenMax)
+		}
+		for p, b := range value {
+			ok := false
+			for _, br := range mode.ValidRanges {
+				if (b >= br.Min) && (b <= br.Max) {
+					ok = true
+					break
+				}
+			}
+			if !ok {
+				log.Fatalf("Byte at pos %d is out of valid range\n", p)
+			}
+		}
+		return fmt.Sprintf("\x1d\x6b%s%s\x00", mode.Code, value)
+	}},
 }
 
 func init() {
@@ -75,7 +122,12 @@ func main() {
 		log.Printf("%T -> '%v': %s\n", tkn, tkn, tkn)
 		switch tt := tkn.(type) {
 		case xml.StartElement:
-			fmt.Print(getTagDef(tt.Name.Local).StartBytes)
+			tag := getTagDef(tt.Name.Local)
+			if tag.ProcFunc != nil {
+				fmt.Print(tag.ProcFunc(tt))
+			} else {
+				fmt.Print(tag.StartBytes)
+			}
 		case xml.EndElement:
 			tag := getTagDef(tt.Name.Local)
 			fmt.Print(tag.EndBytes)
@@ -101,4 +153,17 @@ func getTagDef(rawName string) TagDefintion {
 		log.Fatalln("Unknown token:", name)
 	}
 	return def
+}
+
+func getAttr(e xml.StartElement, name string, required bool) (mode string) {
+	for _, a := range e.Attr {
+		if strings.ToLower(a.Name.Local) == name {
+			mode = a.Value
+			break
+		}
+	}
+	if (mode == "") && required {
+		log.Fatalf("Required attr '%s' not found\n", name)
+	}
+	return mode
 }
